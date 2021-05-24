4df47ebbe6c5e98c57626f2de504a677c2d6f4b9
CarlosOrtiz <carlos.ortiz@udla.edu.co>
2021-05-24 01:53:41 -0500

    feat: add Param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new/:id')
+  newPath(@Param('id') id: string): string {
+    return `Is param: ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  newCategory(
+    @Param('productId') productId: string,
+    @Param('id') id: string,
+  ): string {
+    return `Id the category: ${id} and product is: ${productId}`;
   }
 }
